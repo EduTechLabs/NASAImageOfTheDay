88ec2e7e7d37dde430878418d03bb3481d35916d
vinayKatdare <vinay.katdare@gmail.com>
2014-09-06 14:14:20 +0530

    ID are allocated to buttons and Initilized UI elements in java

--- NASAProject/src/com/example/nasaproject/MainActivity.java
+++ NASAProject/src/com/example/nasaproject/MainActivity.java
@@ -4,15 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
+	private TextView title;
+	private TextView date;
+	private ImageView image;
+	private TextView description;
+	private Button buttonRefresh;
+	private Button buttonSetWallpaper;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initializeUI();
+        
+        
+    }
+    
+    private void initializeUI(){
+    	
+    	title = (TextView) findViewById(R.id.imageTitle);
+    	date = (TextView) findViewById(R.id.imageDate);
+    	image = (ImageView) findViewById(R.id.imageDisplay);
+    	description = (TextView) findViewById(R.id.imageDesc);
+    	buttonRefresh = (Button) findViewById(R.id.button_refresh);
+    	buttonSetWallpaper = (Button) findViewById(R.id.button_setAsWallpaper);
     }
  
 }
